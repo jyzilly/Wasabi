34f0d1a26915dccb731edd78b3cc71ec938b4c15
kjy2000212 <kjy2000212@gmail.com>
2024-12-02 17:53:32 +0900

    Scene1 GameManager 입력테스트 저장,버튼 및 씬 바꾸는 기능 추가

--- Wasabi/Assets/Scripts/GameManager.cs
+++ Wasabi/Assets/Scripts/GameManager.cs
@@ -1,6 +1,31 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+
+    //닉네임 입력을 받아서 저장하고 버튼을 클릭할 때 다음 씬으로 이동하기
+
+    [SerializeField] private TMP_InputField  nickName;
+    [SerializeField] private Button startButton;
+
+
+    private void Start()
+    {
+        PlayerPrefs.SetString("nickName", nickName.text);        
+        startButton.onClick.AddListener(() => SceneManager.LoadScene("Wasabi 2"));
+    }
+
+
+
+
+
+
+
+
+
+
+
 }
